284b3707f0b8a7340c60148c2b21bd941da4994c
dhslrl321 <dhslrl321@gmail.com>
2021-10-04 15:54:50 +0900

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import * as S from "./styles";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <S.Conatiner>
+      create-react-app
+    </S.Conatiner>
   );
 }
 
