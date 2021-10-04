68c8fb7c627140e215a62b19bb4cacc23e5fba88
dhslrl321 <dhslrl321@gmail.com>
2021-10-04 15:44:48 +0900

    commit

---
+++ backend/src/main/java/com/my/s3/SpringS3Application.java
@@ -0,0 +1,13 @@
+package com.my.s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringS3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringS3Application.class, args);
+    }
+
+}
